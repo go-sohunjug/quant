3ac3b375bee7a7bc8f6fd0fff3c43ec36b7bb448
sohunjug <sohunjug@hotmail.com>
2022-02-22 20:23:48 +0800

    sharp

---
+++ model/method.go
@@ -0,0 +1,13 @@
+package model
+
+var (
+	Sharp = map[string]string{
+		"spot": "C",
+		"swap": "S",
+		"future": "F",
+		"option": "O",
+		"aoption": "A",
+		"isolated": "I",
+		"margin": "M",
+	}
+)
